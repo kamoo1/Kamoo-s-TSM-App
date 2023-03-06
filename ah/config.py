0d975a5b7369e743d11a77ec5201092f5ded9eca
kamoo1 <4003844+kamoo1@users.noreply.github.com>
2023-03-07 02:09:42 +0800

    fix reading env

--- ah/config.py
+++ ah/config.py
@@ -3,8 +3,8 @@ import os
 from ah.defs import SECONDS_IN
 from ah.fs import get_temp_path
 
-BN_CLIENT_ID = os.environ("BN_CLIENT_ID")
-BN_CLIENT_SECRET = os.environ("BN_CLIENT_SECRET")
+BN_CLIENT_ID = os.environ.get("BN_CLIENT_ID")
+BN_CLIENT_SECRET = os.environ.get("BN_CLIENT_SECRET")
 DATA_BASE_PATH = get_temp_path()
 APP_NAME = "ah"
 MARKET_VALUE_RECORD_EXPIRES = 60 * SECONDS_IN.DAY
