4a088eacd1f76f42dc58173e9d1af7a9565fd759
kamoo1 <4003844+kamoo1@users.noreply.github.com>
2023-09-24 15:57:18 +0800

    chore: update requirements & coverage, add typing

--- ah/data/__init__.py
+++ ah/data/__init__.py
@@ -1,16 +1,17 @@
 import os
 import json
+from typing import Dict
 
 __all__ = ("map_bonuses",)
 
 
-def load_json(filename):
+def load_json(filename: str) -> Dict:
     path = os.path.join(os.path.dirname(__file__), filename)
     with open(path) as f:
         return json.load(f)
 
 
-def key_to_int(d):
+def key_to_int(d: Dict) -> Dict:
     return {int(k): v for k, v in d.items()}
 
 
