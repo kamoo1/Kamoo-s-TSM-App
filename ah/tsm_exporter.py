2f1799c9ecc7df3fbe58a8a9c9d660af90fb9f28
kamoo1 <4003844+kamoo1@users.noreply.github.com>
2023-10-11 02:54:22 +0800

    fix: regional price sources were unsorted

--- ah/tsm_exporter.py
+++ ah/tsm_exporter.py
@@ -392,6 +392,10 @@ class TSMExporter:
                 # retail = None
                 tsm_region = region
 
+            # need to sort because it's records are from multiple realms
+            # note that we skipped `realm_auctions_commodities_data`, due to
+            # no overlapping item strings between auctions and commodities
+            data = data.sort()
             for region_export in self.REGION_AUCTIONS_COMMODITIES_EXPORTS:
                 self.export_append_data(
                     self.export_file,
