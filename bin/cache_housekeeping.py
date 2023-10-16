e390a145e6c6c694f9122134059eb1801cd28503
kamoo1 <4003844+kamoo1@users.noreply.github.com>
2023-10-17 00:14:54 +0800

    ci: add scripts

---
+++ bin/cache_housekeeping.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import re
+import argparse
+
+
+def move(src, dst):
+    print(f"Moving {src} to {dst}")
+    # we are under same directory
+    os.rename(src, dst)
+
+
+def rename_db_files(db_path: str):
+    mvs = [
+        # 5375
+        ("dynamic-tw_auctions5735.gz", "dynamic-tw_auctions_5735.gz"),
+        # 5376
+        ("dynamic-tw_auctions5736.gz", "dynamic-tw_auctions_5736.gz"),
+        # 963
+        ("dynamic-tw_auctions963.gz", "dynamic-tw_auctions_963.gz"),
+        # 966
+        ("dynamic-tw_auctions966.gz", "dynamic-tw_auctions_966.gz"),
+        # 980
+        ("dynamic-tw_auctions980.gz", "dynamic-tw_auctions_980.gz"),
+        ("dynamic-tw_commodities.gz", "dynamic-tw_commodities.gz"),
+        ("dynamic-tw_meta.json", "dynamic-tw_meta.json"),
+    ]
+    for src, dst in mvs:
+        move(os.path.join(db_path, src), os.path.join(db_path, dst))
+
+
+def exec(command: str):
+    # run bash command
+    print(f"Running command: {command}")
+    os.system(command)
+
+
+def main(db_path: str = None):
+    before_fns = list(os.listdir(db_path))
+    print(f"Before files: {before_fns}")
+    # rename_db_files(db_path)
+    exec(f"cd {db_path} && rm *classic*")
+    before_fns = list(os.listdir(db_path))
+    print(f"After files: {before_fns}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--db_path",
+        type=str,
+        default="/tmp/ah_db",
+    )
+    args = parser.parse_args()
+    main(**vars(args))
---
+++ bin/preprocess_data.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+import argparse
+from typing import Dict
+
+
+class DataPreprocessor:
+    FIELDS_TO_KEEP_BONUSES = {"level", "base_level", "curveId"}
+
+    @classmethod
+    def load_json(cls, path: str):
+        with open(path) as f:
+            return json.load(f)
+
+    @classmethod
+    def dump_json(cls, path: str, data: Dict):
+        with open(path, "w") as f:
+            json.dump(data, f, indent=4)
+
+    @classmethod
+    def add_curve_points(cls, data: Dict, curves: Dict, curve_id: int):
+        curve = curves[str(curve_id)]
+        new_points = []
+        for point in curve["points"]:
+            new_points.append(
+                (
+                    point["playerLevel"],
+                    point["itemLevel"],
+                )
+            )
+        data["points"] = new_points
+        return data
+
+    @classmethod
+    def join_bonuses_and_curves(cls, bonuses: Dict, curves: Dict) -> Dict:
+        new_bonuses = {}
+        # go over all item of bonuses, remove all fields except FIELDS_TO_KEEP_BONUSES
+        for k, v in bonuses.items():
+            # nk = int(k) # it's JSON, we can't use int as key
+            nk = k
+            nv = {k_: v_ for k_, v_ in v.items() if k_ in cls.FIELDS_TO_KEEP_BONUSES}
+            if "curveId" in nv:
+                nv = cls.add_curve_points(nv, curves, nv["curveId"])
+
+            new_bonuses[nk] = nv
+
+        return new_bonuses
+
+    @classmethod
+    def run(cls, data_path: str, output_path: str):
+        data_bonuses = cls.load_json(os.path.join(data_path, "bonuses.json"))
+        data_curves = cls.load_json(os.path.join(data_path, "item-curves.json"))
+        bonuses = cls.join_bonuses_and_curves(data_bonuses, data_curves)
+        cls.dump_json(output_path, bonuses)
+
+
+def main(data_path: str = None, output_path: str = None):
+    DataPreprocessor.run(data_path, output_path)
+
+
+def parse_args(raw_args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", help="Path to data", default="./build", type=str)
+    parser.add_argument(
+        "--output_path",
+        help="Path to output",
+        default="./ah/data/bonuses_curves.json",
+        type=str,
+    )
+    args = parser.parse_args(raw_args)
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(**vars(args))
