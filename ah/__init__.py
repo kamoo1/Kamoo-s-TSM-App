bc2154550f3c3711eab1d8b634f570bfda4b8812
jeeves <semantic-release>
2023-10-18 09:33:14 +0000

    chore(release): 0.1.0 [skip ci]
    Automatically generated by python-semantic-release

--- ah/__init__.py
+++ ah/__init__.py
@@ -1,4 +1,4 @@
 # TIL: python3.2+ no longer requires __init__.py to be present in a package
 # https://stackoverflow.com/questions/448271/what-is-init-py-for
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
